f03fabbd7ea0fbd3aea1ac8e9ad572256f8ca404
Chris Cooksley <christopher.cooksley@gmail.com>
2018-09-24 18:56:14 +0100

    directions now updates whenever the stop list is refreshed

--- lift.rb
+++ lift.rb
@@ -8,13 +8,12 @@ class Floor
     Floor.floors << self # adds new floor to floors list
     @people_waiting = queues[floor_num] # gets the people waiting at that floor from the queues list provided
     @floor_num = floor_num # number of the floor, as an integer
-    @directions = @people_waiting.map{ |person| get_direction(person)}.uniq
+    @directions = @people_waiting.map{ |person| get_direction(person)}.uniq # list of which directions people on the floor want to go to (which 'buttons' have been pushed) # NEED TO BE ABLE TO ALTER DIRECTIONS!!!!!
   end
 
   def get_direction(person_num)
     person_num > @floor_num ? "up" : "down"
   end
-
   attr_accessor :people_waiting, :floor_num, :directions
 end
 
@@ -25,22 +24,24 @@ class Lift
     @direction = "up" # starts the lift going up
     @people = [] # will hold a list of the people in the lift, named according to their destination floor number
     @stop_log = [0] # will hold a list of all the stops made, starting with the ground floor
-    update_stops
   end
 
 # creates/updates a list of the next floors to stop on, in order
   def update_stops
-    @stop_list = @people.dup # add any floor that someone has requested
-    Floor.floors.each { |floor| @stop_list << floor.floor_num if floor.directions.include?(@direction) } # add any floor with people who need to go somewhere (we won't actually let them on unless they're going the right way, though!)
+
+    @stop_list = @people.dup # add any floor that someone in the lift has requested
+    Floor.floors.each do |floor|
+      floor.directions = floor.people_waiting.map{ |person| floor.get_direction(person)}.uniq # must update directions to prevent stopping on irrelevant floors
+      @stop_list << floor.floor_num if floor.directions.include?(@direction)
+    end # add any floor with people who are travelling in the right direction
     @stop_list = @stop_list.select{ |floor| @direction == "up" ? floor > @current_floor.floor_num : floor < @current_floor.floor_num } # filter out floors that aren't on the current path
     @stop_list = @stop_list.uniq.sort_by { |floor| (floor - @current_floor.floor_num).abs} # remove duplicates and sort by  distance from the lift
-
   end
 
   def reverse
     @direction = (@direction == "up") ? "down" : "up"
     puts "Reversing direction! Now going #{@direction}"
-    collect
+    collect # important to do this after reversing, as the conditions for collecting those on the current floor may now be met
   end
 
   def stop(floor)
@@ -53,6 +54,7 @@ class Lift
 
   def collect
     # adds people to list if there's space and they're going in the right direction
+    puts "people waiting: #{@current_floor.people_waiting}"
     people_added = []
     @current_floor.people_waiting.each do |person|
       if (@people.length < @capacity) && (@current_floor.get_direction(person) == @direction)
@@ -75,12 +77,22 @@ class Lift
 
   def operate
     puts "Here we go!\n\n"
-    collect # lift needs to collect people at the beginning
-    until Floor.floors.all?{ |floor| floor.people_waiting.empty?}
+    collect # lift needs to collect people at the beginning, as the current floor is not on the stop list
+
+    # We loop until there is nobody left in the lift or waiting on a floor
+    until Floor.floors.all?{ |floor| floor.people_waiting.empty?} and @people.empty?
       update_stops
+      puts "STOP LIST: #{@stop_list}"
       if @stop_list.empty?
-        # if there's no floor left in the current direction that anyone on the lift wants to go to, we stop at the furthest floor in the current direction with anyone waiting on it, and reverse to sweep from the other direction
-        last_stop = Floor.floors.reject { |floor| floor.people_waiting.empty?}.map { |floor| floor.floor_num}.select{ |floor| @direction == "up" ? floor > @current_floor.floor_num : floor < @current_floor.floor_num }.sort_by { |floor| (floor - @current_floor.floor_num).abs }.last
+        # if there's no floor left in the current direction that anyone on the lift wants to go to, we stop at the furthest floor in the current direction with anyone waiting on it, and reverse to sweep from the other direction.
+        non_empty = Floor.floors.reject { |floor| floor.people_waiting.empty?}.map { |floor| floor.floor_num}
+        puts "non-empty: #{non_empty}"
+        valid_non_empty = non_empty.select{ |floor| @direction == "up" ? floor > @current_floor.floor_num : floor < @current_floor.floor_num }
+        puts "valid non: #{valid_non_empty}"
+        sorted_non_empty = valid_non_empty.sort_by { |floor| (floor - @current_floor.floor_num).abs }
+        puts "sorted non-empty: #{sorted_non_empty}"
+        last_stop = sorted_non_empty.last
+        puts "last stop = #{last_stop}"
         stop(last_stop) unless last_stop == nil
         reverse
       else
@@ -88,10 +100,9 @@ class Lift
       end
     end
     puts "\n\nAll done! Returning to the ground floor."
-    @stop_log << 0 # ends at the ground floor.
+    @stop_log << 0 unless @current_floor.floor_num == 0 # ends at the ground floor.
     @stop_log
   end
-
 end
 
 def the_lift(queues, capacity)
@@ -101,4 +112,4 @@ def the_lift(queues, capacity)
   lift.operate
 end
 
-puts the_lift([ [],   [0,],  [],      [],   [2,],  [3,],  [] ], 5)
+puts the_lift([[], [0, 0, 0, 6], [], [], [], [6, 6, 0, 0, 0, 6], []], 5)
